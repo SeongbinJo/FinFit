a38b09e78000417e18d1fe5d142b4d4f45c1dde9
rmfls0606 <rmfls0606@naver.com>
2024-06-03 10:23:38 +0900

    README파일 추가 및 gitignore생성

---
+++ Saver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Saver
+//
+//  Created by 이상민 on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
