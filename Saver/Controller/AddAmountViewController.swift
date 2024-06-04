5c0111c3f5602c47487a14262d855c5e2aa0a631
hyerim <ax1021@naver.com>
2024-06-04 14:28:58 +0900

    거래 내역 입력창 추가

--- Saver/Controller/AddAmountViewController.swift
+++ Saver/Controller/AddAmountViewController.swift
@@ -27,6 +27,46 @@ class AddAmountViewController: UIViewController {
         
         return titleView
     }()
+    
+    private lazy var dateView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        
+        let dateTitle = UILabel()
+        dateTitle.text = "거래 날짜"
+        dateTitle.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        let dateSelector = UIStackView()
+        // TODO: - 옆으로 넘기는 달력 추가
+        
+        stackView.addArrangedSubview(dateTitle)
+        stackView.addArrangedSubview(dateSelector)
+        
+        return stackView
+    }()
+    
+    private lazy var transactionDetailView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+        let transactionDetailTitle = UILabel()
+        transactionDetailTitle.text = "거래 내역"
+        transactionDetailTitle.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        let transactionDetailTextField = UITextField()
+        transactionDetailTextField.placeholder = "거래 내역을 입력해주세요."
+        
+        stackView.addArrangedSubview(transactionDetailTitle)
+        stackView.addArrangedSubview(transactionDetailTextField)
+        
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,20 +81,22 @@ class AddAmountViewController: UIViewController {
         view.addSubview(self.mainContainer)
         
         mainContainer.addArrangedSubview(titleView)
+        mainContainer.addArrangedSubview(dateView)
+        mainContainer.addArrangedSubview(transactionDetailView)
         
         
         // MARK: - 뷰 위치 잡기
         mainContainer.translatesAutoresizingMaskIntoConstraints = false
         titleView.translatesAutoresizingMaskIntoConstraints = false
+        dateView.translatesAutoresizingMaskIntoConstraints = false
+        transactionDetailView.translatesAutoresizingMaskIntoConstraints = false
         
         let safeArea = view.safeAreaLayoutGuide
         
         NSLayoutConstraint.activate([
             mainContainer.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
             mainContainer.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
-            mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 24),
-            
-            titleView.topAnchor.constraint(equalTo: mainContainer.topAnchor, constant: 20),
+            mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
         ])
     }
     
