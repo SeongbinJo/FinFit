4b0c240b42d8dc1027d7091a46c98edb067960ec
avan31017 <avan31017@gmail.com>
2024-06-06 18:44:08 +0900

    [feature/HomeView] - TransactionTableViewCell 생성

---
+++ Saver/Controller/TransactionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionTableViewCell.swift
+//  Saver
+//
+//  Created by 조성빈 on 6/6/24.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
